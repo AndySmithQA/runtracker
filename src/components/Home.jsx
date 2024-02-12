0c7dc6a8a118bce3e465df90d9bd9e90dfc41911
AndySmithQA <andy.smith@qa.com>
2024-02-12 15:45:42 +0000

    Splits Added

--- src/components/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return(
-    <div className="home">
-        <h2>Welcome</h2>
-        <p>Started as a challenge to myself. Now onto the Skoda Tracking.</p>
-    </div>
-    )
-}
--- src/components/Runs.jsx
+++ src/components/Runs.jsx
@@ -6,13 +6,23 @@ export default function Runs() {
     return (
         <div className="runs">
              <ShowGraph />
+             <div className="times">
             {runs.map(run => (
                <Link to={run.id.toString()} key={run.id}>
                     <p>Time taken - {run.time}</p> <br />
                     <p>On {run.date}</p>
+                    <div className="splits">
+                       
+                       <p>1 - {run.k1}</p> 
+                        <p>2 - {run.k2}</p> 
+                        <p>3 - {run.k3}</p> 
+                        <p>4 - {run.k4}</p> 
+                        <p>5 - {run.k5}</p>
+                    </div>
                </Link> 
+               
             ))}
-           
+           </div>
         </div>
     )
 }
